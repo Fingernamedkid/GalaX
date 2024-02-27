7a1f859e434e421c9bcde757de2b5f44dda4ad2f
RachelSilencieux <2139769@collegemv.qc.ca>
2024-02-27 17:07:43 -0500

    Adjotuer qqc

---
+++ galax-front/src/Component/pages/Films.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Films() {
+    return(
+        <div className="films">
+            <section className="film">
+                <h1 style={{color:"black",marginTop:"100px"}}>Ici on place les films</h1>
+            </section>
+
+        </div>
+    )
+}
+
+export default Films;
