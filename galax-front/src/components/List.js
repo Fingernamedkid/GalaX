7d6019fa36e5a6c73dd2c2fe5091edb00c1adc3e
fingernamedkid <minhtri.trandang2005@gmail.com>
2024-03-05 17:01:37 -0500

    123

--- galax-front/src/components/List.js
+++ galax-front/src/components/List.js
@@ -1,34 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Cards from './Cards.js';
 
-export default function List() {
-    const [movies, setMovies] = useState([]);
-    const [startIndex, setStartIndex] = useState(0);
-
-    useEffect(() => {
-        const API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&with_genres=&api_key=8b64af438dcdf72c27a5df692c7ebf1b';
-        
-        const fetchMovies = async () => {
-            try {
-                const res = await fetch(API_URL);
-                const data = await res.json();
-                setMovies(data.results.slice(0, 25)); 
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
-        };
-    
-        fetchMovies();
-    }, []); 
-    
-    const scrollLeft = () => {
-        setStartIndex(prevIndex => Math.max(prevIndex - 5, 0));
-    };
-
-    const scrollRight = () => {
-        setStartIndex(prevIndex => Math.min(prevIndex + 5, movies.length - 1));
-    };
-
+export default function List({movies, startIndex}) {
     return (
         
                 
