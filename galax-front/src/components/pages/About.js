438b6bb47594ab377e59147b244712f04eeba6ce
2139769 <2139769@collegemv.qc.ca>
2024-04-10 15:10:32 -0400

    Changed

--- galax-front/src/components/pages/About.js
+++ galax-front/src/components/pages/About.js
@@ -1,38 +1,75 @@
 import React from "react";
+import img from "../img/profil.png";
+import img2 from "../img/profilfemme.jpg";
+import { NavLink } from "react-router-dom";
 
 
 export default function About() {
 
-    return(
-        <div className="stats shadow"><font></font>
-  <font></font>
-  <div className="stat"><font></font>
-    <div className="stat-figure text-secondary"><font></font>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg><font></font>
-    </div><font></font>
-    <div className="stat-title">Downloads</div><font></font>
-    <div className="stat-value">31K</div><font></font>
-    <div className="stat-desc">Jan 1st - Feb 1st</div><font></font>
-  </div><font></font>
-  <font></font>
-  <div className="stat"><font></font>
-    <div className="stat-figure text-secondary"><font></font>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg><font></font>
-    </div><font></font>
-    <div className="stat-title">New Users</div><font></font>
-    <div className="stat-value">4,200</div><font></font>
-    <div className="stat-desc">↗︎ 400 (22%)</div><font></font>
-  </div><font></font>
-  <font></font>
-  <div className="stat"><font></font>
-    <div className="stat-figure text-secondary"><font></font>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg><font></font>
-    </div><font></font>
-    <div className="stat-title">New Registers</div><font></font>
-    <div className="stat-value">1,200</div><font></font>
-    <div className="stat-desc">↘︎ 90 (14%)</div><font></font>
-  </div><font></font>
-  <font></font>
-</div>
-    )
+  return(
+      <div className="about-body">
+        <NavLink to="/home"><i className="fa-solid fa-arrow-left" id="arrow-btn"
+                               style={{fontSize:"30px"}}></i></NavLink>
+        <h3 className="aboutus">About us</h3>
+        <br></br>
+        <section className="about-info">
+          <h1 className="title-about">Our website</h1>
+          <p className="para-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
+            Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+            Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          </p>
+        </section>
+        <section className="about-container">
+          <br/>
+          <div class="row">
+            <div className="profil">
+              <img src={img} alt="profile" className="about-image"/>
+              <div className="info-container">
+                <h1>Lorsen</h1>
+                <p class="title">Computer Science Student</p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam tellus.</p>
+                <br/>
+                <p>lorsen@exemple.com</p>
+                <a href="#contact"><button type="button" id="about-btn">Contact me</button></a>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="profil">
+              <img src={img2} alt="profile" className="about-image" style={{width:"300px", height:"160px"}}/>
+              <div className="info-container">
+                <h1>Rachel</h1>
+                <p class="title">Computer Science Student</p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam tellus.</p>
+                <br/>
+                <p>rachel@exemple.com</p>
+                <a href="#contact"><button type="button" id="about-btn">Contact me</button></a>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="profil">
+              <img src={img} alt="profile" className="about-image"/>
+              <div className="info-container">
+                <h1>Tri</h1>
+                <p class="title">Computer Science Student</p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam tellus.</p>
+                <br/>
+                <p>tri@exemple.com</p>
+                <a href="#contact"><button type="button" id="about-btn">Contact me</button></a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <br/>
+
+        <br/>
+
+      </div>
+  )
 }
